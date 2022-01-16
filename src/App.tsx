dd46a42a2035555865bc47bfe134d01e822c3ec6
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-01-16 09:37:09 -0400

    Estilização do modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { createServer } from 'miragejs';
+import Modal from 'react-modal';
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 createServer({
   routes() {
@@ -23,10 +28,27 @@ createServer({
 })
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      
       <Dashboard/>
+      
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      
       <GlobalStyle/>
     </>
   );
